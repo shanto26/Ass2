812b09ea9fe42a131bd765dec96af862babeadd7
M. Zanibul Haque Shanto <71281253+shanto26@users.noreply.github.com>
2022-04-20 23:12:13 +0600

    Add files via upload

---
+++ Python/256x256 Matrix Mul python.py
@@ -0,0 +1,19 @@
+import numpy as np
+from datetime import datetime
+start_time = datetime.now()
+
+x = np.random.randint(10, size=(256,256))
+y = np.random.randint(10, size=(256,256))
+
+r = [[0 for x in range(256)] for y in range(256)]
+
+
+for i in range(len(x)):
+    for j in range(len(y[0])):
+        for k in range(len(y)):
+ 
+            r[i][j] += x[i][k] * y[k][j]
+
+print(r) 
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
